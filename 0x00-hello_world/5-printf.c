2588016bb73c1d9bcd691b79bbbfbc6d08bb7176
fabioyame <ochogbe.fabian@gmail.com>
2023-03-17 00:14:31 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
-* main - Entry point
-* Return: always 0    
-*/
+ * main - Entry point
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
