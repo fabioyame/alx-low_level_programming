90a2591788bd7c8b15a9012841f8d911dbf97473
fabioyame <ochogbe.fabian@gmail.com>
2023-03-21 20:26:08 -0700

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,7 +17,7 @@ unsigned long half1, half2;
 for (count = 0; count < 92; count++)
 {
 	sum = fib1 +fib2;
-	prinf("%lu, ", sum);
+	printf("%lu, ", sum);
 	fib1 = fib2;
 	fib2 = sum;
 }
@@ -28,10 +28,11 @@ fib2_half2 = fib2 % 10000000000;
 for (count = 93; count < 99; count++)
 {
 	half1 = fib1_half1 + fib2_half1;
-	half2 = (fib1_half2 + fib2_half2 > 9999999999)
+	half2 = (fib1_half2 + fib2_half2;
+	if (fib1_half2 + fib2_half2 > 9999999999)
 	{
-		half1 += 1;
-		half2 %= 10000000000;
+	half1 += 1;
+	half2 %= 10000000000;
 	}
 	printf("%lu%lu", half1, half2);
 	if (count != 98)
