57a23671e70241bbee9e3af317ff3b0a26ec779c
fabioyame <ochogbe.fabian@gmail.com>
2023-03-27 22:29:31 +0000

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- *print_rev - print reverse string
+ * print_rev - print reverse a string
  *
  * @s: string to print
  * Return: 0
  */
 void print_rev(char *s)
 {
-int i;
-
-for (i = _strlen(s) - 1; i >= 0; i--)
-{
-_putchar(*(s + i));
-}
+int start = 0;
+while (s[start])
+start++;
+while (start--)
+_putchar(s[start]);
 _putchar('\n');
 }
