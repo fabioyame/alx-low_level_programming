852f83f35d4099fd41316c546d13528c441529c9
fabioyame <ochogbe.fabian@gmail.com>
2023-03-27 23:21:31 +0000

    7 put

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,7 +10,7 @@ void puts_half(char *str)
 {
 	int full_string, half_string;
 full_string = 0;
-while (str[full_string] != ‘\0')
+while (str[full_string] != '\0')
 full_string++;
 half_string = full_string / 2;
 if (full_string % 2 == 1)
