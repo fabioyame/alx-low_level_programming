42005123a55c542782eef04945fdc524151617b2
fabioyame <ochogbe.fabian@gmail.com>
2023-03-27 22:21:59 +0000

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+i#include "main.h"
+
+/**
+ *print_rev - print reverse string
+ *
+ * @s: string to print
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+int i;
+
+for (i = _strlen(s) - 1; i >= 0; i--)
+{
+_putchar(*(s + i));
+}
+_putchar('\n');
+}
