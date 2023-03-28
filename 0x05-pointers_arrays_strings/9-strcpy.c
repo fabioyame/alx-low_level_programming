0fe0d2dbf07dbf143c3814e793ae4ff0f262836f
fabioyame <ochogbe.fabian@gmail.com>
2023-03-28 00:08:10 +0000

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy - main function to copy
+ *
+ * @dest: destination to copy
+ * @src: char type string
+ *
+ * Return: a character value
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i = -1;
+
+do {
+i++;
+dest[i] = src[i];
+}
+while (src[i] != '\0');
+return (dest);
+}
