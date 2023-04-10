592ee10dc9d4729dca92c50e2bb4fa3b622d4d87
fabioyame <ochogbe.fabian@gmail.com>
2023-04-10 10:54:28 +0000

    coin

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
- * main - The main function to print
+ * main - The main function to print the minimum number
  * minimum number of coins
  * @argc: Argument count
  * @argv: Vector of arguments
@@ -12,40 +13,39 @@
 
 int main(int argc, char  *argv[])
 {
-	int coins = 0;
-int money;
-	if (argc == 2)
-	{
-		if (strchr(argv[argc - 1], '-'))
-		{
-			printf("0\n");
-			return (1);
-		}
-				money = atoi(argv[argc - 1]);
+	int money, coins = 0;
 
-		while (money > 0)
-		{
-			if (money % 25 == 0)
-			{
-				money -= 25;
-			} else if (money % 10 == 0)
-			{
-				money -= 10;
-			} else if (money % 5 == 0)
-			{
-				money -= 5;
-			} else if (money % 2 == 0)
-			{
-				money -= 2;
-			} else
-			{
-				money--;
-			}
-			coins++;
-		}
-		printf("%d\n", coins);
-		return (0);
-	}
-	printf("Error\n");
-	return (1);
+	if (argc != 2)
+	{
+		printf("Error\n");
+return (1);
+}
+money = atoi(argv[1]);
+while (money > 0)
+{
+coins++;
+if ((money - 25) >= 0)
+{
+money -= 25;
+continue;
+}
+if ((money - 10) >= 0)
+{
+money -= 10;
+continue;
+}
+if ((money - 5) >= 0)
+{
+money -= 5;
+continue;
+}
+if ((money - 2) >= 0)
+{
+money -= 2;
+continue;
+}
+money--;
+}
+printf("%d\n", coins);
+return (0);
 }
