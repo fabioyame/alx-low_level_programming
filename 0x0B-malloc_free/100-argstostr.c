36910a2a85e633ea05d4bbd58da0a7885b20841b
fabioyame <ochogbe.fabian@gmail.com>
2023-04-12 00:00:03 +0000

    argstos

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,39 +8,33 @@
  * Return: 0
  */
 char *argstostr(int ac, char **av)
+
 {
-	int size;
-	char *s;
-	int m;
-	int n;
-	int p;
+int i, n, r = 0, l = 0;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
-
-	for (m = 0; m < ac; m++)
-	{
-		for (n = 0; av[m][n] != '\0'; n++)
-			size++;
-	}
-
-	size += (ac + 1);
-	s = malloc(sizeof(char) * size);
-	if (s == NULL)
-		return (NULL);
-	p = 0;
-	for (m = 0; m < ac; m++)
-	{
-		for (n = 0; av[m][n] != '\0'; n++)
-		{
-			s[p] = av[m][n];
-			p++;
-		}
-		s[p] = '\n';
-		p++;
-	}
-
-	s[p] = '\0';
-
-	return (s);
+char *str;
+if (ac == 0 || av == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+for (n = 0; av[i][n]; n++)
+l++;
+}
+l += ac;
+str = malloc(sizeof(char) * l + 1);
+if (str == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+for (n = 0; av[i][n]; n++)
+{
+str[r] = av[i][n];
+r++;
+}
+if (str[r] == '\0')
+{
+str[r++] = '\n';
+}
+}
+return (str);
 }
