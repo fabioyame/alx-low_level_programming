ccee5d076e683477561ff013abd2b10165a8524b
fabioyame <ochogbe.fabian@gmail.com>
2023-04-17 14:58:09 +0000

    filename

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - The main file returns file name
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
